0611e113ef05cd3c136db69cb9d6962c3a21af4b
Naina Alex <naina.alex1@tvsmotor.com>
2022-01-28 13:54:15 +0530

    First commit

---
+++ Rule_based_info_ext.py
@@ -0,0 +1,16 @@
+import re
+import string
+import nltk
+import spacy
+import pandas as pd
+import numpy as np
+import math
+from tqdm import tqdm
+
+from spacy.matcher import Matcher
+from spacy.tokens import Span
+from spacy import displacy
+
+#load spaCy model
+
+nlp = spacy.load("en_core_web_sm")
